91916576df0b19a156c81e1e87101d1993291ea4
Bora-Ulu <74740445+Bora-Ulu@users.noreply.github.com>
2020-12-23 16:23:31 +0300

    Added the refining process of the y vector
    The computation is a bit slow. Probably, it can be improved with njit and functools.

--- Inequalities_v2.py
+++ Inequalities_v2.py
@@ -15,13 +15,12 @@ import mosek
 from itertools import *
 import sympy as sy
 from scipy.sparse import coo_matrix, csc_matrix, csr_matrix
-#from matplotlib.pyplot import *
 
 DataInstrumental=[0.5,0,0,0,0,0.5,0,0]
 TriangleData=[0.12199995751046305, 0.0022969343799089472, 0.001748319476328954, 3.999015242496535e-05, 0.028907881434196828, 0.0005736087488455967, 0.0003924033706699725, 1.1247230369521505e-05, 0.0030142577390317635, 0.09234476010282468, 4.373922921480586e-05, 0.0014533921021948346, 0.0007798079722868244, 0.024091567451515063, 1.1247230369521505e-05, 0.0003849052170902915, 0.020774884184769502, 0.000396152447459813, 0.0003049249122403608, 4.998769053120669e-06, 0.10820335492385, 0.0020794879260981982, 0.0015546171755205281, 2.4993845265603346e-05, 0.0006260958239033638, 0.020273757587194154, 7.498153579681003e-06, 0.0003374169110856452, 0.0028942872817568676, 0.08976414557915113, 2.624353752888351e-05, 0.0012984302615480939, 0.002370666223442477, 4.7488306004646356e-05, 0.0999928767540993, 0.001957018084296742, 0.0006198473625869629, 8.747845842961171e-06, 0.02636975644747481, 0.0005198719815245496, 1.4996307159362007e-05, 0.000403650601039494, 0.0005498645958432735, 0.017359475229224805, 7.123245900696953e-05, 0.002346922070440154, 0.0033754188031197316, 0.10295964618712641, 0.00038740460161685187, 7.498153579681003e-06, 0.01608353942841575, 0.000306174604503641, 0.0021319750011559654, 4.248953695152569e-05, 0.09107007399427891, 0.001860791780024169, 5.998522863744803e-05, 0.0018395470115484063, 0.002570616985567304, 0.0766411271224461, 1.874538394920251e-05, 0.00048238121362614454, 0.0006410921310627258, 0.020223769896662948]
 
 inflation_order=2
-card=2
+card=4
 TriangleGraph=Graph.Formula("X->A:C,Y->A:B,Z->B:C")
 EvansGraph=Graph.Formula("U3->A:C:D,U2->B:C:D,U1->A:B,A->C,B->D")
 InstrumentalGraph=Graph.Formula("U1->X->A->B,U2->A:B")
@@ -31,14 +30,16 @@ try:
     importlib.reload(inflation)
 except NameError:
     import InitializeInflation as inflation
-    
 
-EncodedA = inflation.InflationMatrixFromGraph(InstrumentalGraph,inflation_order,card)
-CVXOPT=inflation.FormCVXOPTArrayFromOnesPositions(EncodedA)
+obs_count,num_vars,exp_set,group_elem,det_assumptions,names=inflation.LearnInflationGraphParameters(TriangleGraph,inflation_order)
 
-b=inflation.FindB(DataInstrumental,inflation_order)
+valid_column_orbits=inflation.ValidColumnOrbitsFromGraph(TriangleGraph,inflation_order,card)
 
-Sol,epsilon=inflation.InflationLP(EncodedA,b)
+SpMatrix=inflation.SparseInflationMatrix(obs_count, num_vars, valid_column_orbits, exp_set, inflation_order, card)
+
+b=inflation.FindB(TriangleData,inflation_order)
+
+Sol,epsilon=inflation.InflationLP(SpMatrix,b)
 
 
 def Compatibility(Sol,b,epsilon):
@@ -56,16 +57,85 @@ def Compatibility(Sol,b,epsilon):
 
     return Comp
 
+def Epsilon(y,n):
+    
+    ymax=max(y)
+    denom=1000*n
+    epsilon=ymax/denom
+    
+    return epsilon
 
-def Inequality(Graph,inflation_order,card,Sol,b,epsilon):
+def ValidityCheck(y,SpMatrix):
+    
+    Smatrix=SpMatrix.toarray()    
+    checkY=y.T.dot(Smatrix)
+    
+    if np.amin(checkY) >= 0:
+        
+        Validity=True
+        
+    else:
+        
+        Validity=False
+        
+    return Validity
+    
+
+def yZeroFilter(Sol,n):
+    
+    y=np.array(Sol)
+    epsilon=Epsilon(y,n)
+    
+    for i, element in enumerate(y):
+        if element <= epsilon:
+            y[i]=0
+
+    return y
+
+def yRoundFilter(y,n):
+    
+    epsilon=Epsilon(y,n)
+    order=-np.floor(np.log10(epsilon))
+    
+    for i in range(len(y)):
+        
+        y[i]=int((10**(order))*y[i])
+        
+    uy=np.unique(y).astype(np.uint)
+    GCD=np.gcd.reduce(list(uy))
+    
+    y=y/GCD
+    
+    return y   
+    
+
+def Inequality(Graph,inflation_order,card,Sol,b,SpMatrix):
+    
+    y=np.array(Sol)
+    
+    n=1
+    
+    epsilon=Epsilon(y,n)
+    
+    y=yZeroFilter(Sol,n)
+    
+    while ValidityCheck(y,SpMatrix) == False:
+        
+        n=n*10
+        y=yZeroFilter(Sol,n)
+        
+    y=yRoundFilter(y,n)
+    
+    while ValidityCheck(y,SpMatrix) == False:
+        
+        n=n*10
+        y=yRoundFilter(Sol,n)
     
     Comp=Compatibility(Sol,b,epsilon)
     
     if Comp == False:
-    
-        y=np.array(Sol)
         
-        obs_count,num_vars,exp_set,group_elem,det_assumptions,names=inflation.LearnInflationGraphParameters(InstrumentalGraph,inflation_order)
+        obs_count,num_vars,exp_set,group_elem,det_assumptions,names=inflation.LearnInflationGraphParameters(Graph,inflation_order)
         G=inflation.GenerateEncodingMonomialToRow(card**obs_count,inflation_order)
     
         st=''.join(str(i) for i in np.arange(card))
@@ -81,7 +151,6 @@ def Inequality(Graph,inflation_order,card,Sol,b,epsilon):
         for i in range(1,inflation_order):
             bs=np.kron(preb,bs)
     
-        G=inflation.GenerateEncodingMonomialToRow(card**obs_count,inflation_order)
     
         ListingProbs3=[]
         for i in np.unique(G):
@@ -94,10 +163,16 @@ def Inequality(Graph,inflation_order,card,Sol,b,epsilon):
                 elem=elem+bs[indecies[j]]   
             ListingProbs3.append(elem)
         ListingProbs3=np.array(ListingProbs3)
-           
+        
+        y=y.astype(np.uint)
+        
         InequalityAsArray=y.T.dot(ListingProbs3)
-    
-        InequalityAsString=str(InequalityAsArray[0])+'>=0'
+        
+        divisor=sy.content(InequalityAsArray[0])
+        
+        Inequality=InequalityAsArray[0]/divisor
+        
+        InequalityAsString=str(Inequality)+'>=0'
         
         return InequalityAsArray,InequalityAsString
     else:
@@ -105,9 +180,9 @@ def Inequality(Graph,inflation_order,card,Sol,b,epsilon):
 
 
 
-Graph=InstrumentalGraph
-IA,IS=Inequality(Graph,inflation_order,card,Sol,b,epsilon)
-y=np.array(Sol)
+Graph=TriangleGraph
+IA,IS=Inequality(Graph,inflation_order,card,Sol,b,SpMatrix)
+
 #print(SOL)
 print(IS)
 
