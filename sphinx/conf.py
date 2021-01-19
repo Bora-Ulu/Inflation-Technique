e62acca16fca2c4c2a0f522e77bb615b61ec4eb4
Elie Wolfe <elupus@gmail.com>
2021-01-19 11:54:35 -0500

    change relative path to root directory

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.split(__file__)[0] + '../../'))
+sys.path.insert(0, os.path.abspath(os.path.split(__file__)[0] + '../'))
 
 import lib
 #import lib.inflationmatrix
